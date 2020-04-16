ee4d0e48adc71561d6cc44cdcddcc74e24cf5a87
mester-pid <41203575+mester-pid@users.noreply.github.com>
2020-04-16 23:50:38 +0430

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def index():
     if request.method == 'POST':
         plink = request.form['text']
         #res = subprocess.check_output(["wget","-O","static/p.mp4", plink])
+        res = subprocess.check_output(["rm","-f","static/p.mp4"])
         res = subprocess.check_output(["youtube-dl","-o","./static/p.mp4", plink])   
         print(res)
         return render_template('index.html', link="./static/p.mp4",log=res)
