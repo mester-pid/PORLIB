af9412cb585795100fb917f3ee33c3953aa11233
mester-pid <41203575+mester-pid@users.noreply.github.com>
2020-04-16 20:54:22 +0430

    show wget log

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
         plink = request.form['text']
         res = subprocess.check_output(["wget","-O","./static/p.mp4", plink])  
         print(res)
-        return render_template('index.html', link="/static/p.mp4")
+        return render_template('index.html', link="/static/p.mp4",log=res)
         
     return render_template('index.html', link="/static/p.mp4")
     
